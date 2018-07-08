c927bdb726b7ac04e2eb5817ee169653cc7b3a8f
natew <natew@192.168.0.11>
2018-07-08 19:56:12 -0400

    added application class test

--- src/main/java/edu/villanova/csc/health/Application.java
+++ src/main/java/edu/villanova/csc/health/Application.java
@@ -1,12 +1,12 @@
-package edu.villanova.csc.healthsvc;
+package edu.villanova.csc.health;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HealthSvcApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HealthSvcApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
--- src/test/java/edu/villanova/csc/health/ApplicationTest.java
+++ src/test/java/edu/villanova/csc/health/ApplicationTest.java
@@ -1,16 +1,24 @@
-package edu.villanova.csc.healthsvc;
+package edu.villanova.csc.health;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class HealthSvcApplicationTests {
-
+public class ApplicationTest {
+	
+	@Mock
+	private Application app;
+	
+	@SuppressWarnings("static-access")
 	@Test
 	public void contextLoads() {
+		String[] args = new String[0];
+		Mockito.verify(app).main(args);
 	}
 
 }
